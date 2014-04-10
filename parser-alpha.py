e52f8cc320dde974eb1a38c69581ae7e5b897e04
wmarquesr <wesleymr.27@gmail.com>
2014-04-10 16:28:37 -0300

    Initial commit

---
+++ parser-alpha.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+tokens = ['IDENTIFIER',
+          'LPAREN', 'RPAREN',
+          'LBRACE', 'RBRACE',
+          'LBRACKET', 'RBRACKET',
+          'ASSIGN_SIGN'
+          'PLUS_SIGN', 'MINUS_SIGN', 'MULTIPLY_SIGN', 'DIVIDE_SIGN', 'MODULUS_SIGN',
+          'EQUALS_SIGN', 'DIFF_SIGN', 'NEG_SIGN', 'AND', 'OR'
+          'LT_SIGN', 'GT_SIGN', 'LTE_SIGN', 'GTE_SIGN',
+          'INT_LIT', 'BOOL_LIT', 'LIST_LIT', 'STR_LIT',
+          'COMMA', 'SEMICOLON']
+
+reserved = {
+          'int': 'INT',
+          'bool': 'BOOL'
+          'string': 'STRING'
+          'void': 'VOID'
+}
+
+import ply.lex as lex
+lex.lex()
+
+# gramar
+
+import ply.yacc as yacc
+yacc.yacc()
+
+# compiler
